3ac886c6c6e85cd59eb6e7ee811eccd606c958aa
GSushi <gmarcieloliveira@gmail.com>
2021-06-18 12:57:22 -0300

    feat: resolution of the PrudentteTech challenge

--- desafio-1/src/bank/index.ts
+++ desafio-1/src/bank/index.ts
@@ -1,16 +1,73 @@
 import { User } from '../mock';
 
-export function teste1(users: User[], user_id: string): number {}
-
-export function teste2(users: User[], info: ITest2): Test2Respose {}
-
 type ITest2 = {
   password?: string;
   amount?: number;
   user_id: string;
 };
 
-type Test2Respose = {
+type Test2Response = {
   success: boolean;
   message?: string;
 };
+
+
+export function teste1(users: User[], user_id: string): number {
+  const userBalance = users.find((user) => user_id === user.id);
+  const balance = userBalance.balance;
+
+  return balance;
+}
+
+export function teste2(users: User[], info: ITest2): Test2Response {
+  const userInfo = users.find((user) => info.user_id === user.id);
+  if (userInfo != null) {
+    if (userInfo.age != null && userInfo.balance != null
+      && userInfo.id != null && userInfo.name != null && userInfo.password != null) {
+
+      if (userInfo.password === info.password) {
+        if (userInfo.balance >= info.amount) {
+          const Test2Responsed = {
+            success: true,
+            message: "Operação de pagamento aprovada!"
+          }
+
+          return Test2Responsed;
+        }
+        else {
+          const Test2Responsed = {
+            success: false,
+            message: 'Saldo insuficiente'
+          }
+
+          return Test2Responsed;
+        }
+      }
+      else {
+        const Test2Responsed = {
+          success: false,
+          message: 'Senha incorreta'
+        }
+
+        return Test2Responsed;
+      }
+    } else {
+      const Test2Responsed = {
+        success: false,
+        message: 'Dados do usuário não encontrados!'
+      }
+
+      return Test2Responsed;
+    }
+  }
+  else {
+    const Test2Responsed = {
+      success: false,
+      message: 'Usuário não encontrado!'
+    }
+
+    return Test2Responsed;
+  }
+}
+
+
--- desafio-1/src/geral/index.ts
+++ desafio-1/src/geral/index.ts
@@ -1,5 +1,25 @@
 import { User } from '../mock';
 
-export function teste1(numbers: number[]): number[] {}
+export function teste1(numbers: number[]): number[] {
+  const findDuplicates = (numbers) => {
+    let sorted_numbers = numbers.slice().sort();
 
-export function teste2(users: User[]): User {}
+    let results = [];
+    for (let i = 0; i < sorted_numbers.length - 1; i++) {
+      if (sorted_numbers[i + 1] == sorted_numbers[i]) {
+        results.push(sorted_numbers[i]);
+      }
+    }
+    return results;
+  }
+
+  const arrUnique = [... new Set(findDuplicates(numbers))];
+  const orderedNumbers = arrUnique.sort((a, b) => a - b);
+  return orderedNumbers;
+}
+
+export function teste2(users: User[]): User {
+  const nomeProcurado = users.find((user) => user.name.includes('Alvares Cabral'));
+
+  return nomeProcurado;
+}
